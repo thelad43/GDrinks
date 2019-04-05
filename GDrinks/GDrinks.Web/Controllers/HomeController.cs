2ced8f506ecec2a499aa033416157c49f4ae7570
Teodor <thelad43@gmail.com>
2019-04-05 12:30:40 +0300

    Code refactored

--- GDrinks/GDrinks.Web/Controllers/HomeController.cs
+++ GDrinks/GDrinks.Web/Controllers/HomeController.cs
@@ -1,25 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using GDrinks.Web.Models;
-
-namespace GDrinks.Web.Controllers
+﻿namespace GDrinks.Web.Controllers
 {
+    using GDrinks.Web.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Diagnostics;
+
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        [HttpGet]
+        public IActionResult Index() => View();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        [HttpGet]
+        public IActionResult Privacy() => View();
 
+        [HttpGet]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
