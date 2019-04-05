5ccbfb61efcb23147b4994099eff15218bcba63f
Teodor <thelad43@gmail.com>
2019-04-05 12:24:11 +0300

    Created project

---
+++ GDrinks/GDrinks.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GDrinks.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
