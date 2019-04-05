adc52f5f9b7bbbb923acf0b5a428785472c5f272
Teodor <thelad43@gmail.com>
2019-04-05 14:34:47 +0300

    Scaffolded identity

---
+++ GDrinks/GDrinks.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GDrinks.Data;
+using GDrinks.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GDrinks.Web.Areas.Identity.IdentityHostingStartup))]
+namespace GDrinks.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
